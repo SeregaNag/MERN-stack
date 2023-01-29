0127c2533fdece3fa6dff127e384957bfc225a80
SeregaNag <71101232+SeregaNag@users.noreply.github.com>
2023-01-29 15:59:53 +0300

    Express Router & API Routes

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,19 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
+const express = require("express");
+const workoutRoutes = require("./routes/workouts");
 
-const app = express()
+const app = express();
+
+app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
-app.get('/', (req, res) => {
-  res.json({mssg: 'Welcome'})
-})
+app.use("/api/workouts", workoutRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log("listening on port", process.env.PORT);
 });
-
